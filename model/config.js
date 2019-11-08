195f2417c9e0c789bb664b3baa7a47e1ee6dd1dc
Lechenco <glechenco@gmail.com>
2019-11-08 15:49:26 -0300

    Atlas

--- model/config.js
+++ model/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    uri: 'mongodb://localhost:27017/projeto-web2',
+    uri: 'mongodb+srv://admin:makoto12345@cluster0-hhbc3.mongodb.net/test?retryWrites=true&w=majority',
     db: 'projeto-web2',
     options: {
         useNewUrlParser: true
