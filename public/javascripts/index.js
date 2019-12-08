c6284abb4696b6bef2890bf63757efb0bd083bce
Lechenco <glechenco@gmail.com>
2019-12-08 18:25:53 -0300

    live search autocomplete suggestion

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,11 +7,16 @@ function liveSearch(value) {
                     data = response.data,
                     listItens = "";
                 data.map((elem) => {
-                    listItens += '<li class="live-search-item">' + elem + '</li>\n'
+                    listItens += '<li class="live-search-item" onclick="clickLiveSearch(this)">' 
+                                + elem + '</li>\n';
                 })
                 list.innerHTML = listItens;
-
         })
     else
         document.querySelector("ul.live-search").innerHTML = "";
+}
+
+function clickLiveSearch(elem) {
+    document.querySelector("input.search-input").value = elem.innerHTML;
+    document.querySelector("ul.live-search").innerHTML = "";
 }
